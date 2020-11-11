e0716f546cb265f403f04a250680df44583b0a40
cjmugs <cjmugs@gmail.com>
2020-11-10 20:17:50 -0600

    updates

--- SinglePageApp/SinglePage/static/SinglePage/page.js
+++ SinglePageApp/SinglePage/static/SinglePage/page.js
@@ -11,3 +11,19 @@ document.addEventListener('DOMContentLoaded' , function(){
         }
     };
 });
+
+function showPage(page) {
+    document.querySelectorAll('div').forEach(div=> {
+        div.style.display = 'none';
+    })
+
+    document.querySelector(`#${page}`).style.display = 'block';
+}
+
+    document.addEventListener('DOMContentLoaded', function(){
+         document.querySelectorAll('button').forEach(button => {
+             button.onclick = function(){
+                 showPage(this.dataset.page);
+             }
+         });
+    });
